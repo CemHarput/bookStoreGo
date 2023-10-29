8b0d76065e41600ebdd95af9ef5bd0649603a177
CemHarput <cemharput@gmail.com>
2023-10-29 19:56:06 +0300

    First Commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+func main() {
+	app := fiber.New()
+	
+	app.Get("/", func(ctx *fiber.Ctx) error {
+	   return ctx.JSON(fiber.Map{
+		  "message": "hello world!",
+	   })
+	})
+	
+	log.Fatal(app.Listen(":8080"))
+ }
