b36b8d1dd0dc428e5d4758a7fe2d07cb34941375
CemHarput <cemharput@gmail.com>
2023-11-12 19:48:47 +0300

    Basic Crud Logic for Book Entity is added

--- main.go
+++ main.go
@@ -3,16 +3,21 @@ package main
 import (
 	"log"
 
+	"github.com/CemHarput/bookStoreGo/config"
 	"github.com/gofiber/fiber/v2"
 )
 func main() {
 	app := fiber.New()
 	
-	app.Get("/", func(ctx *fiber.Ctx) error {
-	   return ctx.JSON(fiber.Map{
-		  "message": "hello world!",
-	   })
-	})
+	config.Connect()
+
+	api := app.Group("/api/v1")
+
+    api.Get("/books", handlers.getBooks)
+    api.Get("/books/:id", handlers.getBook)
+    api.Post("/books", handlers.addBook)
+    api.Put("/books/:id", handlers.updateDog)
+    api.Delete("/books/:id", handlers.removeDog)
 	
-	log.Fatal(app.Listen(":8080"))
+	log.Fatal(app.Listen(":3000"))
  }
